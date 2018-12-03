f6e62dd69f9e57904e1f4ee5dbf4291fcc82ae33
Kyle Morse <kmorse@rallydev.com>
2018-12-03 15:36:05 -0700

    Day 2 Part 2

--- 2018/2/checksum.js
+++ 2018/2/checksum.js
@@ -8,7 +8,7 @@ function countChars(word) {
     }, {});
 }
 
-inputUtils.getInput().then(function(input) {
+function computeChecksum(input) {
     const counts = input.map(countChars);
     
     let twos = 0;
@@ -24,5 +24,44 @@ inputUtils.getInput().then(function(input) {
         }
     });
 
-    console.log('Checksum:', twos * threes);
+    return twos * threes;
+}
+
+function findCommonId(input) {
+    let diff;
+    input.some(function(str1, i) {
+        if (diff) {
+            return true;
+        }
+        input.some(function(str2, j) {
+            if (diff) {
+                return true;
+            }
+
+            const str1Chars = [...str1];
+            const str2Chars = [...str2];
+            let position = 0;
+            const diffs = str1Chars.reduce(function(accum, letter, index) {
+                if (str1Chars[index] !== str2Chars[index]) {
+                    accum++;
+                    position = index;
+                }
+                return accum;
+            }, 0);
+
+            if (diffs === 1) {
+                diff = str1.substring(0, position) + str1.substring(position+1);
+            }
+        });
+    });
+
+    return diff;
+}
+
+inputUtils.getInput().then(function(input) {
+    const checksum = computeChecksum(input);
+    console.log('Checksum:', checksum);
+
+    const id = findCommonId(input);
+    console.log('Common ID:', id);
 });
