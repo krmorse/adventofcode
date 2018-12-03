9bc7b2c93bd0d0e1a6c06df315ace7a5f190c2eb
Kyle Morse <kmorse@rallydev.com>
2018-12-03 10:00:17 -0700

    Day 1, Part 2

--- 2018/1/frequency.js
+++ 2018/1/frequency.js
@@ -1,5 +1,5 @@
 const readline = require('readline');
-let frequency = 0;
+const frequencyDeltas = [];
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,7 +8,38 @@ const rl = readline.createInterface({
 });
 
 rl.on('line', function (line) {
-    const currentFrequency = frequency;
-    frequency += parseInt(line, 10);
-    console.log(`Current frequency: ${currentFrequency}, change of ${line}; resulting frequency: ${frequency}.`)
+  frequencyDeltas.push(parseInt(line, 10));
 });
+
+rl.on('close', function () {
+  const firstPassFrequency = findFirstPassFrequency();
+  console.log('\nFrequency after first pass:', firstPassFrequency);
+
+  const firstDupe = findFirstFrequencyDupe();
+  console.log('\nFirst frequency dupe:', firstDupe);
+});
+
+function findFirstPassFrequency() {
+  return frequencyDeltas.reduce(function(accum, value) {
+    return value + accum;
+  }, 0);
+}
+
+function findFirstFrequencyDupe() {
+  let dupeFound = false;
+  let index = 0;
+  let frequency = 0;
+  let frequencies = {};
+
+  while (!dupeFound) {
+    if (frequencies[frequency]) {
+      dupeFound = true;
+    } else {
+      frequencies[frequency] = true;
+      frequency += frequencyDeltas[index % frequencyDeltas.length]
+      index++;
+    }
+  }
+
+  return frequency;
+}
