02709a791d38c9606eff9a0065b15c3cff8863af
Kyle Morse <kmorse@rallydev.com>
2018-12-03 12:51:22 -0700

    Day 2, Part 1

--- 2018/1/frequency.js
+++ 2018/1/frequency.js
@@ -1,7 +1,33 @@
 const inputUtils = require('../utils/input');
+let frequencyDeltas;
+
+function findFirstPassFrequency() {
+  return frequencyDeltas.reduce(function(accum, value) {
+    return value + accum;
+  }, 0);
+}
+
+function findFirstFrequencyDupe() {
+  let dupeFound = false;
+  let index = 0;
+  let frequency = 0;
+  let frequencies = {};
+
+  while (!dupeFound) {
+    if (frequencies[frequency]) {
+      dupeFound = true;
+    } else {
+      frequencies[frequency] = true;
+      frequency += frequencyDeltas[index % frequencyDeltas.length]
+      index++;
+    }
+  }
+
+  return frequency;
+}  
 
 inputUtils.getInput().then(function(input) {
-  const frequencyDeltas = input.map(function (line) {
+  frequencyDeltas = input.map(function (line) {
     return parseInt(line, 10);
   });
   
@@ -10,29 +36,4 @@ inputUtils.getInput().then(function(input) {
   
   const firstDupe = findFirstFrequencyDupe();
   console.log('\nFirst frequency dupe:', firstDupe);
-  
-  function findFirstPassFrequency() {
-    return frequencyDeltas.reduce(function(accum, value) {
-      return value + accum;
-    }, 0);
-  }
-  
-  function findFirstFrequencyDupe() {
-    let dupeFound = false;
-    let index = 0;
-    let frequency = 0;
-    let frequencies = {};
-  
-    while (!dupeFound) {
-      if (frequencies[frequency]) {
-        dupeFound = true;
-      } else {
-        frequencies[frequency] = true;
-        frequency += frequencyDeltas[index % frequencyDeltas.length]
-        index++;
-      }
-    }
-  
-    return frequency;
-  }  
 });
--- 2018/2/checksum.js
+++ 2018/2/checksum.js
@@ -1,5 +1,28 @@
 const inputUtils = require('../utils/input');
 
+function countChars(word) {
+    const chars = [...word];
+    return chars.reduce(function(accum, c) {
+        accum[c] = (accum[c] || 0) + 1;
+        return accum;
+    }, {});
+}
+
 inputUtils.getInput().then(function(input) {
+    const counts = input.map(countChars);
     
+    let twos = 0;
+    let threes = 0;
+
+    counts.forEach(function(count) {
+        const countString = Object.values(count).join('');
+        if (countString.indexOf('3') >= 0) {
+            threes++;
+        }
+        if (countString.indexOf('2') >= 0) {
+            twos++;
+        }
+    });
+
+    console.log('Checksum:', twos * threes);
 });
