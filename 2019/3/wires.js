8869d5bc7183e780e3b58a12aa2be0b53ac906f6
Kyle Morse <kyle.morse@workday.com>
2019-12-03 18:55:12 -0700

    day 3 part 2

--- 2019/3/wires.js
+++ 2019/3/wires.js
@@ -4,12 +4,13 @@ const plotWire = (wireLabel, wire, coords) => {
     let x = 0;
     let y = 0;
     
+    let steps = 0;
     for(let i = 0; i < wire.length; i++) {
         const section = wire[i];
         for(let j = 0; j < section.length; j++) {
-            coords[`${x},${y}`] = coords[`${x},${y}`] || { size: 0, distance: Math.abs(x) + Math.abs(y)};
-            if (!coords[`${x},${y}`][wireLabel]) {
-                coords[`${x},${y}`][wireLabel] = true;
+            coords[`${x},${y}`] = coords[`${x},${y}`] || { steps: {}, size: 0, distance: Math.abs(x) + Math.abs(y)};
+            if (!coords[`${x},${y}`].steps[wireLabel]) {
+                coords[`${x},${y}`].steps[wireLabel] = steps;
                 coords[`${x},${y}`].size++;
             }
            
@@ -27,6 +28,7 @@ const plotWire = (wireLabel, wire, coords) => {
                     y--;
                     break;
             }
+            steps++;
         }
     }
 };
@@ -41,10 +43,15 @@ inputUtils.getInput().then((wires) => {
     wires.forEach((wire, i) => plotWire(`wire${i+1}`, parseWire(wire), coords));
     delete coords['0,0'];
     
-    const result = Object.values(coords).sort((a, b) => {
-        const diff = b.size - a.size;
-        return diff == 0 ? a.distance - b.distance : diff;
+    const result = Object.values(coords)
+    .filter(coord => coord.size === wires.length)
+    .map(coord => {
+        coord.totalSteps = Object.values(coord.steps).reduce((accum, val) => accum + val, 0)
+        return coord;
+    })
+    .sort((a, b) => {
+        return a.totalSteps - b.totalSteps;
     });
     
-    console.log('distance:', result[0].distance);
+    console.log('total steps:', result[0].totalSteps);
 });
