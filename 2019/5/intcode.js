a6cdc4611dab507007aacc97446e2df6f21db9ac
Kyle Morse <kyle.morse@workday.com>
2019-12-05 09:58:30 -0700

    day5 part 1

--- 2019/5/intcode.js
+++ 2019/5/intcode.js
@@ -1,27 +1,43 @@
 const inputUtils = require('../utils/input');
 
-const compute = async (program) => {
+const readValue = (program, value, mode) => {
+    return !!mode ? value : program[value];
+};
+
+const writeValue = (program, index, mode, value) => {
+    program[index] = value;
+};
+
+const compute = (program) => {
     let index = 0, done = false;
     while(!done) {
-        switch(program[index]) {
+        const opCode = program[index] % 100;
+        const paramModes = program[index].toString().split('').reverse().slice(2).map(p => parseInt(p, 10));
+        switch(opCode) {
             case 99:
                 done = true;
                 break;
             case 1:
-                program[program[index+3]] = program[program[index+1]] + program[program[index+2]];
+                const addValue = readValue(program, program[index+1], paramModes[0]) + 
+                    readValue(program, program[index+2], paramModes[1]);
+                writeValue(program, program[index+3], paramModes[2], addValue);
                 index += 4;
                 break;
             case 2:
-                program[program[index+3]] = program[program[index+1]] * program[program[index+2]];
+                const multiplyValue = readValue(program, program[index+1], paramModes[0]) * 
+                    readValue(program, program[index+2], paramModes[1]);
+                writeValue(program, program[index+3], paramModes[2], multiplyValue);
                 index += 4;
                 break;
             case 3:
                 const input = inputUtils.prompt('Input: ');
-                program[program[index+1]] = parseInt(input, 10);
+                writeValue(program, program[index+1], 0, parseInt(input, 10));
                 index += 2;
+                break;
             case 4:
-                console.log('Output:', program[program[index+1]]);
+                console.log('Output:', readValue(program, program[index+1], paramModes[0]));
                 index += 2;
+                break;
         }
     }
     return program[0];
