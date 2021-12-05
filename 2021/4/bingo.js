f8942ffaea8e67e845fd79ba8a75543dc3bcb383
Kyle Morse <kyle.morse@workday.com>
2021-12-05 15:41:37 -0700

    2021 day 4 part 2

--- 2021/4/bingo.js
+++ 2021/4/bingo.js
@@ -28,40 +28,51 @@ const isWinningBoard = (board) => {
   for (let y = 0; y < 5; y++) {
     if (board[y].every(isNaN)) {
       return true;
-    }  
+    }
   }
 
   //check columns
   for (let x = 0; x < 5; x++) {
-    const col = [board[0][x], board[1][x], board[2][x], board[3][x], board[4][x]];
+    const col = [
+      board[0][x],
+      board[1][x],
+      board[2][x],
+      board[3][x],
+      board[4][x],
+    ];
     if (col.every(isNaN)) {
       return true;
     }
   }
 
   return false;
-}
+};
 
 const part1 = () => {
   inputUtils.getInput().then((input) => {
     const { callNumbers, boards } = parseInput(input);
 
-    callNumbers.every(callNumber => {
-      boards.forEach(board => {
+    callNumbers.every((callNumber) => {
+      boards.forEach((board) => {
         board.forEach((row, y) => {
           row.forEach((item, x) => {
             if (item === callNumber) {
               board[y][x] = NaN;
             }
-          })
+          });
         });
-      }); 
-      
+      });
+
       const winningBoards = boards.filter(isWinningBoard);
       if (winningBoards.length) {
         const winningBoard = winningBoards[0];
         const remainingTotal = winningBoard.reduce((accum, row) => {
-          return accum + row.filter(val => !isNaN(val)).reduce((accum, val) => accum + val, 0)
+          return (
+            accum +
+            row
+              .filter((val) => !isNaN(val))
+              .reduce((accum, val) => accum + val, 0)
+          );
         }, 0);
 
         console.log(remainingTotal * callNumber);
@@ -73,4 +84,49 @@ const part1 = () => {
   });
 };
 
-part1();
+const part2 = () => {
+  inputUtils.getInput().then((input) => {
+    const { callNumbers, boards } = parseInput(input);
+
+    let results = [];
+    boards.forEach((board, i) => {
+      callNumbers.every((callNumber, round) => {
+        board.forEach((row, y) => {
+          row.forEach((item, x) => {
+            if (item === callNumber) {
+              board[y][x] = NaN;
+            }
+          });
+        });
+
+        if (isWinningBoard(board)) {
+          const remainingTotal = board.reduce((accum, row) => {
+            return (
+              accum +
+              row
+                .filter((val) => !isNaN(val))
+                .reduce((accum, val) => accum + val, 0)
+            );
+          }, 0);
+
+          results.push({
+            turns: round,
+            callNumber: callNumber,
+            remainingTotal,
+          });
+
+          return false;
+        }
+
+        return true;
+      });
+    });
+
+    results.sort((a, b) => b.turns - a.turns);
+
+    const longestBoard = results[0];
+    console.log(longestBoard.callNumber * longestBoard.remainingTotal);
+  });
+};
+
+part2();
