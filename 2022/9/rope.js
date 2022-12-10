ee2f42ed86ad76cc3a7616b087024fb218ec842a
Kyle Morse <kyle.morse@workday.com>
2022-12-10 08:37:46 -0700

    2022 day 9 part 2 wip

--- 2022/9/rope.js
+++ 2022/9/rope.js
@@ -64,9 +64,39 @@ const part1 = () => {
 };
 
 const part2 = () => {
+  const KNOTS = 10;
   inputUtils.getInput().then((inputLines) => {
+    let positions = [];
+    for (let i = 0; i < KNOTS; i++) {
+      positions.push({x: 0, y: 0});
+    };
 
+    const head = positions[0];
+    const tail = positions[positions.length-1];
+    let tailPositions = { '0,0': true };
+
+    inputLines.forEach(move => {
+      const [direction, count] = move.split(' ');
+      for (let i = 0; i < parseInt(count, 10); i++) {
+        if (direction === 'R') {
+          head.x++;
+        } else if (direction === 'L') {
+          head.x--;
+        } else if (direction === 'U') {
+          head.y--;
+        } else {
+          head.y++;
+        }
+
+        for (let i = 1; i < positions.length; i++) {
+          calculateNewTailPosition(positions[i-1], positions[i]);
+        }
+        tailPositions[`${tail.x},${tail.y}`] = true;
+      }
+    });
+
+    console.log(Object.keys(tailPositions).length);
   });
 };
 
-part1();
+part2();
