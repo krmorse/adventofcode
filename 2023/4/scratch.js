bd8e9479e49b46c5bf0aa0f9ff1d714903e46869
Kyle Morse <kyle.morse@workday.com>
2023-12-04 10:40:56 -0700

    2023 day 4 part 2

--- 2023/4/scratch.js
+++ 2023/4/scratch.js
@@ -14,9 +14,35 @@ const part1 = async () => {
   console.log(winPoints.reduce((result, wp) => result + wp, 0));
 };
 
-const part2 = async () => {
-  const input = await getInput();
-  
+const cardMemo = {};
+
+const compute = (cards, card) => {
+  if (cardMemo[card.cardNumber]) {
+    return cardMemo[card.cardNumber];
+  }
+
+  const cardsToProcess = cards.slice(card.cardNumber, card.cardNumber + card.matches.length)
+  cardMemo[card.cardNumber] = cardsToProcess.length + cardsToProcess.reduce((result, c) => result + compute(cards, c), 0);
+  return cardMemo[card.cardNumber];
 };
 
-part1();
+const part2 = async () => {
+  const input = await getInput();
+  const cards = input.map((card, i) => {
+    const numbers = card.split(':')[1].trim();
+    const [winnerString, mineString] = numbers.split('|');
+    const winners = winnerString.trim().split(' ').map(w => parseInt(w, 10)).filter(w => !isNaN(w));
+    const mine = mineString.trim().split(' ').map(w => parseInt(w, 10)).filter(w => !isNaN(w));
+    const matches = mine.filter(m => winners.includes(m));
+    return {
+      cardNumber: i+1,
+      winners,
+      mine,
+      matches
+    };
+  });
+
+  console.log(cards.length + cards.reduce((result, c) => result + compute(cards, c), 0));
+};
+
+part2();
